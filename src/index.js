71aa9325ac8d571f5dddbf3e8392fbcbce928f29
Saran <tansleepless@gmail.com>
2019-11-20 22:06:54 +0700

    Prepare to do frontend

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+//connect to db
+mongoose.connect('mongodb+srv://Admin:tantrum@cluster0-gfhpd.gcp.mongodb.net/Streaddit?retryWrites=true&w=majority', { useUnifiedTopology: true,useNewUrlParser: true,useFindAndModify: false })
+
+//body passing for postman
+app.use(express.json())
+
+//set routes
+const tagRoute = require('./routes/tag')
+
+//all routes are called here
+app.use(tagRoute)
+
+app.listen(8080, () => {
+  console.log('Server is running.')
+})
---
+++ src/routes/tag.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const router = express.Router()
+
+// model for data
+const tagModel = require('./models/tagModel')
+
+//show all tags
+router.get('/tag',async (req,res)=>{
+  const tag = await tagModel.find({})
+  try{
+    res.send(tag)
+  }catch(err){
+    res.status(500).send(err)
+  }
+})
+
+// add tag
+router.post('/tag',async (req,res)=>{
+    const tag = new tagModel(req.body)
+    try{
+      await tag.save()
+      res.send(tag)
+    }catch(err){
+      res.status(500).send(err)
+    }
+  })
+
+// delete tag
+router.delete('/tag/:id',async (req,res)=>{
+  try{
+    const tag = await tagModel.findByIdAndDelete(req.params.id)
+    if(!tag){
+      res.status(404).send("No item found")
+    }
+    res.status(200).send()
+  }catch(err){
+    res.status(500).send(err)
+  }
+})
+
+// update tag
+router.patch('/tag/:id',async (req,res)=>{
+  try{
+    const tag = await tagModel.findByIdAndUpdate(req.params.id,req.body)
+    await tag.save()
+    res.send(tag)
+  }catch(err){
+    res.status(500).send(err)
+  }
+})
+
+module.exports = router
