8ef789cd87b64d6cbcd701f732a36e477cf2a324
Saran <tansleepless@gmail.com>
2019-11-24 14:59:25 +0700

    Add router for frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <TagComponent/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import TagComponent from './components/TagComponent.vue'
 
 export default {
   name: 'app',
-  components: {
-    TagComponent
-  }
 }
 </script>
 
