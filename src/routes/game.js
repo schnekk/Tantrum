39fc05a1727adf9be380540f4125c56de890590d
Saran <tansleepless@gmail.com>
2019-11-28 17:45:59 +0700

    กฟห

--- src/routes/game.js
+++ src/routes/game.js
@@ -6,7 +6,7 @@ const tagModel = require('../models/tagModel')
 const devModel = require('../models/devModel')
 const gameModel = require('../models/gameModel')
 
-//show all games
+//show all games //
 router.get('/game',async (req,res)=>{
   const game = await gameModel.find({})
   try{
@@ -34,7 +34,7 @@ router.post('/game',async (req,res)=>{
   }
 })
 
-// show a game's tag
+// show a game's tag //
 router.get('/game/:id/tag',async (req,res)=>{
   try{
     const {id} = req.params;
@@ -45,7 +45,20 @@ router.get('/game/:id/tag',async (req,res)=>{
   }
 })
 
-// add tags to a game
+router.get('/tag/:id/game',async (req,res)=>{
+  try{
+    const {id} = req.params;
+    gameModel.findById(id).populate('game').exec((err,tag)=>{
+      tag.game=tag.game.filter(element=>element!=null)
+      res.send(tag)
+      tag.save()
+    })
+  }catch(err){
+    res.status(500).send(err)
+  }
+})
+
+// add tags to a game and add a game to tags
 router.post('/game/:id/tag',async (req,res)=>{
   try{
     const {id} = req.params
@@ -62,9 +75,10 @@ router.post('/game/:id/tag',async (req,res)=>{
   }catch(err){
     res.status(500).send(err)
   }
-})
+}
+)
 
-// add dev to a game
+// add dev to a game //
 router.post('/game/:id/developer',async (req,res)=>{
   try{
     const {id} =req.params
@@ -81,18 +95,25 @@ router.post('/game/:id/developer',async (req,res)=>{
 // delete game
 router.delete('/game/:id',async (req,res)=>{
   try{
-    const game = await gameModel.findByIdAndDelete(req.params.id)
+    const {id} = req.params
+    const game = await gameModel.findById(id)
+    tagModel.update(
+      {_id:{$in: game.tag}},
+      {$pull: {game: game._id}},
+      {multi: true}
+    )
+    //await gameModel.findByIdAndDelete(id)
     if(!game){
       res.status(404).send("No item found")
     }else{
-      res.status(200).send(game)
+      res.status(200).send("Success")
     }
   }catch(err){
     res.status(500).send(err)
   }
 })
 
-// update game
+// update game 
 router.patch('/game/:id',async (req,res)=>{
   try{
     const game = await gameModel.findByIdAndUpdate(req.params.id,req.body)
@@ -100,11 +121,12 @@ router.patch('/game/:id',async (req,res)=>{
       res.status(404).send("No item found")
     }else{
       await game.save()
-      res.status(200).send(game)
+      res.status(200).send("Success")
     }
   }catch(err){
     res.status(500).send(err)
   }
 })
+
 
 module.exports = router
