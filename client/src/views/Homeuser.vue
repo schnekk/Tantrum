e014fb08a8c07f9e8569129efbbbbdcfc22df56c
kames2542 <yosapake-p@hotmail.com>
2019-12-02 00:27:06 +0700

    add photo to page and home

--- client/src/views/Homeuser.vue
+++ client/src/views/Homeuser.vue
@@ -83,41 +83,41 @@
    <v-container fluid>
     <v-row justify="space-around">
       <v-col cols="3">
-        <v-img src="./assets/monsterhunter.png" aspect-ratio="2"></v-img>
-        <div class="ma-2"><hl class="display-1">Monters Hunter</hl></div>
+          <v-img :src="require('@/assets/DeathStrading.jpg')" aspect-ratio="2"></v-img>
+        <div class="ma-2"><hl class="display-1">Death Strading</hl></div>
         <v-btn text class="ml-4" >INFO</v-btn>
         <v-btn text class="" >FORUM</v-btn>
       </v-col>
       <v-col cols="3">
-        <v-img src="./assets/monsterhunter.png" aspect-ratio="2"></v-img>
-       <div class="ma-2"><hl class="display-1">Monters Hunter</hl></div>
+          <v-img :src="require('@/assets/anthem.jpg')" aspect-ratio="2"></v-img>
+       <div class="ma-2"><hl class="display-1">Anthem</hl></div>
         <v-btn text class="ml-4" >INFO</v-btn>
         <v-btn text class="" >FORUM</v-btn>
       </v-col>
      
       <v-col cols="3">
-        <v-img src="./assets/monsterhunter.png" aspect-ratio="2"></v-img>
-       <div class="ma-2"><hl class="display-1">Monters Hunter</hl></div>
+          <v-img :src="require('@/assets/AssassinOdyssey.png')" aspect-ratio="2"></v-img>
+       <div class="ma-2"><hl class="display-1">Assassin Creed Odyssey</hl></div>
         <v-btn text class="ml-4" >INFO</v-btn>
         <v-btn text class="" >FORUM</v-btn>
       </v-col>
      </v-row>
        <v-row justify="space-around">
      <v-col cols="3">
-        <v-img src="./assets/monsterhunter.png" aspect-ratio="2"></v-img>
-       <div class="ma-2"><hl class="display-1">Monters Hunter</hl></div>
+          <v-img :src="require('@/assets/tomclancy.jpg')" aspect-ratio="2"></v-img>
+       <div class="ma-2"><hl class="display-1">The Division 2</hl></div>
         <v-btn text class="ml-4" >INFO</v-btn>
         <v-btn text class="" >FORUM</v-btn>
       </v-col>
        <v-col cols="3">
-        <v-img src="./assets/monsterhunter.png" aspect-ratio="2"></v-img>
-       <div class="ma-2"><hl class="display-1">Monters Hunter</hl></div>
+          <v-img :src="require('@/assets/gta5.jpg')" aspect-ratio="2"></v-img>
+       <div class="ma-2"><hl class="display-1">GTA V</hl></div>
         <v-btn text class="ml-4" >INFO</v-btn>
         <v-btn text class="" >FORUM</v-btn>
       </v-col>
       <v-col cols="3">
-        <v-img src="./assets/monsterhunter.png" aspect-ratio="2"></v-img>
-       <div class="ma-2"><hl class="display-1">Monters Hunter</hl></div>
+          <v-img :src="require('@/assets/reddead2.jpg')" aspect-ratio="2"></v-img>
+       <div class="ma-2"><hl class="display-1">Red Dead Redemption 2</hl></div>
         <v-btn text class="ml-4" >INFO</v-btn>
         <v-btn text class="" >FORUM</v-btn>
       </v-col>
